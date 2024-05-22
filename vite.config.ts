993c4e3ba64be1f8bb6deef169478802c44396ce
amir-roshan <amirvalielahi@gmail.com>
2024-05-21 20:17:14 -0700

    Importing commonjs in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import commonjs from "@rollup/plugin-commonjs";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), commonjs()],
   optimizeDeps: {
-    include: ["axios", "websocket"], // Include websocket and any other commonjs libraries you might be using
+    include: ["axios", "websocket"], // Add other CommonJS libraries as needed
   },
 });

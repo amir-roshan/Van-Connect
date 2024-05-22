84096bea3eb12bc68151069cbacff312a1da27ee
amir-roshan <amirvalielahi@gmail.com>
2024-05-21 20:44:18 -0700

    including react chat engine to use common js

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import commonjs from "@rollup/plugin-commonjs";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), commonjs()],
   optimizeDeps: {
-    include: ["axios", "websocket"], // Include other CommonJS libraries if needed
+    include: ["axios", "react-chat-engine"],
   },
   build: {
     commonjsOptions: {

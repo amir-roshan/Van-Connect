1dc9e5016592fe518be0c4ebbd972d072f48f119
amir-roshan <amirvalielahi@gmail.com>
2024-05-21 20:33:34 -0700

    adding transformMixedEsModules to true

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import commonjs from "@rollup/plugin-commonjs";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), commonjs()],
   optimizeDeps: {
-    include: ["axios", "websocket"], // Add other CommonJS libraries as needed
+    include: ["axios", "websocket"], // Include other CommonJS libraries if needed
+  },
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
   },
 });

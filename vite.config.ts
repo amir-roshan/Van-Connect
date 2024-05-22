634487480e111f8a7a9443a98abf96954037916d
amir-roshan <amirvalielahi@gmail.com>
2024-05-21 20:04:24 -0700

    Adding common js module

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import commonjs from "@rollup/plugin-commonjs";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), commonjs()],
+  optimizeDeps: {
+    include: ["axios", "websocket"], // Include websocket and any other commonjs libraries you might be using
+  },
 });
